cff4069651b60b63f3678c8ad7b5b0c1655a4fa6
dragonbiketoursvn@gmail.com <71959718+dragonbiketoursvn@users.noreply.github.com>
2021-11-01 12:03:46 +0700

    Fixed path
    Added full path for 'Admin/BikeValuations/getRecord'

--- app/Controllers/Admin/BikeValuations.php
+++ app/Controllers/Admin/BikeValuations.php
@@ -29,7 +29,7 @@ class BikeValuations extends \App\Controllers\BaseController
         
         $this->model->save($bikeValuation);
         
-        return redirect()->to('getRecord');
+        return redirect()->to('Admin/BikeValuations/getRecord');
     }
     
 }
