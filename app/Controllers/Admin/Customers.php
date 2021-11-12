a2b46769ca6710130ce19601e2c147dca5a5bd09
dragonbiketoursvn@gmail.com <71959718+dragonbiketoursvn@users.noreply.github.com>
2021-11-12 13:32:07 +0700

    Update Customers.php

--- app/Controllers/Admin/Customers.php
+++ app/Controllers/Admin/Customers.php
@@ -358,7 +358,7 @@ class Customers extends \App\Controllers\BaseController
   public function  viewAllCustomers()
   {
     $model = new \App\Models\CustomersModel;
-    $customers = $model->limit(1000)->getAllCustomers();
+    $customers = $model->limit(200)->getAllCustomers();
 
     return view('Admin/Customers/viewAllCustomers', ['customers' => $customers]);
   }
