00af0791813a048e036b7d4100c44e9cdbbb44b2
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2022-04-05 14:21:05 +0700

    Debugging Session Issue
    Determining whether in fact we are able to set persistent session variables at all.

--- app/Controllers/Admin/Home.php
+++ app/Controllers/Admin/Home.php
@@ -7,6 +7,7 @@ class Home extends \App\Controllers\BaseController
 	public function index()
 	{
 		session()->remove('appointment');
+		session()->set('test', 'session test');
 		return view('Admin/Home/index');
 	}
 }
