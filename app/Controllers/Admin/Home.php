8c6748fb384b885987bc2ce75c7232883e7cc2ff
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2022-04-05 11:22:09 +0700

    Debug issues with appointment session variable
    Removing appointment variable in Admin/Home controller. Seemed like problems were arising from appointments that were started but not finished, leaving appointment variable in $_SESSION.

--- app/Controllers/Admin/Home.php
+++ app/Controllers/Admin/Home.php
@@ -6,6 +6,7 @@ class Home extends \App\Controllers\BaseController
 {
 	public function index()
 	{
+		session()->remove('appointment'); // clear appointment session variable if it's there
 		return view('Admin/Home/index');
 	}
 }
