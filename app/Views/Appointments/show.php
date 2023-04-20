b7ee594a40fedefcf5e2d7e1c909d0ad89eb8eb7
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2023-04-20 13:45:25 +0700

    Update show.php
    Updated view Appointments/show to reduce width of 'cancel' button (causing issues in mobile display.

--- app/Views/Appointments/show.php
+++ app/Views/Appointments/show.php
@@ -5,43 +5,42 @@
 <?= $this->section("content") ?>
 
 <?php
-  //Should these variables be in the controller?
-  $time = new \DateTime('09:00');
-  $date = new DateTime('2001-10-01');
-  //$addInterval = new DateInterval('P10Y1M1D');
-  //$date->add($addInterval);
+//Should these variables be in the controller?
+$time = new \DateTime('09:00');
+$date = new DateTime('2001-10-01');
+//$addInterval = new DateInterval('P10Y1M1D');
+//$date->add($addInterval);
 
-  //dd($scheduledAppointments);
+//dd($scheduledAppointments);
 
-  $alreadyBookedArray = [];
-  $currentUsersAppointment = null;
+$alreadyBookedArray = [];
+$currentUsersAppointment = null;
 
-  //$check = !empty($scheduledAppointments);
-  //dd($check);
+//$check = !empty($scheduledAppointments);
+//dd($check);
 
-if(!empty($scheduledAppointments)) {
+if (!empty($scheduledAppointments)) {
 
-  foreach($scheduledAppointments as $scheduledAppointment) {
+  foreach ($scheduledAppointments as $scheduledAppointment) {
 
-      if($scheduledAppointment->activation_hash === hash_hmac('sha256', $token, $_ENV['HASH_SECRET_KEY'])) {
-        $currentUsersAppointment = $scheduledAppointment;
-      }
+    if ($scheduledAppointment->activation_hash === hash_hmac('sha256', $token, $_ENV['HASH_SECRET_KEY'])) {
+      $currentUsersAppointment = $scheduledAppointment;
+    }
 
-      $alreadyBooked = new \DateTime($scheduledAppointment->appointment_time);
-      $addInterval = new DateInterval('PT30M');
+    $alreadyBooked = new \DateTime($scheduledAppointment->appointment_time);
+    $addInterval = new DateInterval('PT30M');
 
 
-      $alreadyBooked->sub($addInterval);
-      $alreadyBookedArray[] = $alreadyBooked->format('Y-m-d H:i:s');
-      $alreadyBooked->add($addInterval);
-      $alreadyBookedArray[] = $alreadyBooked->format('Y-m-d H:i:s');
-      $alreadyBooked->add($addInterval);
-      $alreadyBookedArray[] = $alreadyBooked->format('Y-m-d H:i:s');
+    $alreadyBooked->sub($addInterval);
+    $alreadyBookedArray[] = $alreadyBooked->format('Y-m-d H:i:s');
+    $alreadyBooked->add($addInterval);
+    $alreadyBookedArray[] = $alreadyBooked->format('Y-m-d H:i:s');
+    $alreadyBooked->add($addInterval);
+    $alreadyBookedArray[] = $alreadyBooked->format('Y-m-d H:i:s');
   }
-
 }
 
-  //dd($currentUsersAppointment->appointment_time);
+//dd($currentUsersAppointment->appointment_time);
 
 ?>
 
@@ -50,55 +49,53 @@ if(!empty($scheduledAppointments)) {
     <th>
 
     </th>
-    <?php for($i = 1; $i < 8; $i++) {
-      echo '<th>' . date('D, m-d', time() + $i * 24 *3600) . '</th>';
-    }?>
+    <?php for ($i = 1; $i < 8; $i++) {
+      echo '<th>' . date('D, m-d', time() + $i * 24 * 3600) . '</th>';
+    } ?>
 
   </tr>
 
   <?php
-    $time = new \DateTime('08:30');
-    $time2 = new \DateTime('09:00');
-    $addInterval = new DateInterval('PT30M');
+  $time = new \DateTime('08:30');
+  $time2 = new \DateTime('09:00');
+  $addInterval = new DateInterval('PT30M');
 
-    for($i=0; $i < 16; $i++) {
+  for ($i = 0; $i < 16; $i++) {
     $time->add($addInterval);
     $time2->add($addInterval);
 
     echo "<tr> \r\n <td>" . $time->format('G:i') . ' - ' . $time2->format('G:i') . "</td> \r\n";
 
-    for($j=1; $j < 8; $j++) {
+    for ($j = 1; $j < 8; $j++) {
       echo '<td><button';
 
-            $dateString = date('Y-m-d', time() + $j * 24 *3600) . " " . $time->format('H:i:s');
+      $dateString = date('Y-m-d', time() + $j * 24 * 3600) . " " . $time->format('H:i:s');
 
-            if($currentUsersAppointment !== null) {
-              if($dateString == $currentUsersAppointment->appointment_time) {
-                echo ' class="alreadyBooked" disabled';
-              }
-            }
+      if ($currentUsersAppointment !== null) {
+        if ($dateString == $currentUsersAppointment->appointment_time) {
+          echo ' class="alreadyBooked" disabled';
+        }
+      }
 
-            if((((date('l', time() + $j * 24 *3600) == 'Sunday'))) || (in_array($dateString, $alreadyBookedArray)) || ($currentUsersAppointment !== null)) {
-              echo ' class="sunday" disabled';
-            }
+      if ((((date('l', time() + $j * 24 * 3600) == 'Sunday'))) || (in_array($dateString, $alreadyBookedArray)) || ($currentUsersAppointment !== null)) {
+        echo ' class="sunday" disabled';
+      }
 
-            if($currentUsersAppointment !== null) {
-              if($dateString === $currentUsersAppointment->appointment_time) {
-                echo '>Booked</button>
+      if ($currentUsersAppointment !== null) {
+        if ($dateString === $currentUsersAppointment->appointment_time) {
+          echo '>Booked</button>
                 </td>';
-              } else {
-                echo '>Select</button>
+        } else {
+          echo '>Select</button>
                 </td>';
-              }
-            } else {
-              echo '>Select</button></td>';
-            }
-
-
+        }
+      } else {
+        echo '>Select</button></td>';
+      }
     }
 
     echo "</tr>\r\n";
-    }
+  }
   ?>
 
 </table>
@@ -110,55 +107,53 @@ if(!empty($scheduledAppointments)) {
         <th>
 
         </th>
-        <?php for($i = 1; $i < 8; $i++) {
-          echo '<th>' . date('D, m-d', time() + $i * 24 *3600) . '</th>';
-        }?>
+        <?php for ($i = 1; $i < 8; $i++) {
+          echo '<th>' . date('D, m-d', time() + $i * 24 * 3600) . '</th>';
+        } ?>
 
       </tr>
 
       <?php
-        $time = new \DateTime('08:30');
-        $time2 = new \DateTime('09:00');
-        $addInterval = new DateInterval('PT30M');
+      $time = new \DateTime('08:30');
+      $time2 = new \DateTime('09:00');
+      $addInterval = new DateInterval('PT30M');
 
-        for($i=0; $i < 16; $i++) {
+      for ($i = 0; $i < 16; $i++) {
         $time->add($addInterval);
         $time2->add($addInterval);
 
         echo "<tr> \r\n <td>" . $time->format('G:i') . ' - ' . $time2->format('G:i') . "</td> \r\n";
 
-        for($j=1; $j < 8; $j++) {
+        for ($j = 1; $j < 8; $j++) {
           echo '<td><button';
 
-                $dateString = date('Y-m-d', time() + $j * 24 *3600) . " " . $time->format('H:i:s');
+          $dateString = date('Y-m-d', time() + $j * 24 * 3600) . " " . $time->format('H:i:s');
 
-                if($currentUsersAppointment !== null) {
-                  if($dateString == $currentUsersAppointment->appointment_time) {
-                    echo ' class="alreadyBooked" disabled';
-                  }
-                }
+          if ($currentUsersAppointment !== null) {
+            if ($dateString == $currentUsersAppointment->appointment_time) {
+              echo ' class="alreadyBooked" disabled';
+            }
+          }
 
-                if((((date('l', time() + $j * 24 *3600) == 'Sunday'))) || (in_array($dateString, $alreadyBookedArray)) || ($currentUsersAppointment !== null)) {
-                  echo ' class="sunday" disabled';
-                }
+          if ((((date('l', time() + $j * 24 * 3600) == 'Sunday'))) || (in_array($dateString, $alreadyBookedArray)) || ($currentUsersAppointment !== null)) {
+            echo ' class="sunday" disabled';
+          }
 
-                if($currentUsersAppointment !== null) {
-                  if($dateString === $currentUsersAppointment->appointment_time) {
-                    echo '>Booked</button>
+          if ($currentUsersAppointment !== null) {
+            if ($dateString === $currentUsersAppointment->appointment_time) {
+              echo '>Booked</button>
                     </td>';
-                  } else {
-                    echo '>Select</button>
+            } else {
+              echo '>Select</button>
                     </td>';
-                  }
-                } else {
-                  echo '>Select</button></td>';
-                }
-
-
+            }
+          } else {
+            echo '>Select</button></td>';
+          }
         }
 
         echo "</tr>\r\n";
-        }
+      }
       ?>
 
     </table>
@@ -169,10 +164,10 @@ if(!empty($scheduledAppointments)) {
 <?php
 
 
-  if($currentUsersAppointment !== null) {
-    echo form_open(site_url('/appointments/delete/' . $token)) .
-    '<input type="hidden" name="appointment_start" value=""><button class="button">Cancel and Reschedule Current Appointment</button></form>';
-  }
+if ($currentUsersAppointment !== null) {
+  echo form_open(site_url('/appointments/delete/' . $token)) .
+    '<input type="hidden" name="appointment_start" value=""><button class="button">Cancel</button></form>';
+}
 ?>
 
 <?= $this->endSection() ?>
