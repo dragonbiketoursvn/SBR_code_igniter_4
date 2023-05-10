60dfbeaa7980526ab1591badd7041860720a5c17
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2023-05-10 10:16:03 +0700

    Just change to indentation

--- app/Controllers/Admin/Payments.php
+++ app/Controllers/Admin/Payments.php
@@ -9,141 +9,132 @@ use PHPMailer\PHPMailer\Exception;
 
 class Payments extends \App\Controllers\BaseController
 {
-    private $model;
+  private $model;
 
-    public function __construct()
-    {
-        $this->model = new \App\Models\PaymentsModel;
+  public function __construct()
+  {
+    $this->model = new \App\Models\PaymentsModel;
+  }
+
+  public function makeNew()
+  {
+
+    $appointment = session()->get('appointment');
+
+    //This is prevent multiple payments being recorded accidentally
+    if ($appointment->paid_rent == 1) {
+
+      return redirect()->to(site_url('Admin/Appointments/bikeStatusCheck'));
     }
+    return view('Admin/Payments/makeNew', ['appointment' => $appointment]);
+  }
 
-    public function makeNew()
-    {
+  public function savePayment()
+  {
 
-      $appointment = session()->get('appointment');
-      
-      //This is prevent multiple payments being recorded accidentally
-      if($appointment->paid_rent == 1) {
+    //Record that payment was made in appointment record
+    $model = new \App\Models\AppointmentsModel;
+    $appointment = session()->get('appointment');
 
-        return redirect()->to(site_url('Admin/Appointments/bikeStatusCheck'));
+    if ($appointment->paid_rent === 1) {
 
-      }
-      return view('Admin/Payments/makeNew', ['appointment' => $appointment]);
-    }
+      return redirect()->to(site_url('Admin/Appointments/bikeStatusCheck'));
+    } else {
 
-    public function savePayment()
-    {
+      $post = $this->request->getPost();
+      $payment = new Payment($post);
+      //$contract_number = $post['contract_number'];
 
-      //Record that payment was made in appointment record
-      $model = new \App\Models\AppointmentsModel;
-      $appointment = session()->get('appointment');
+      //validation
+      if ($this->model->save($payment) === false) {
 
-      if($appointment->paid_rent === 1) {
-
-        return redirect()->to(site_url('Admin/Appointments/bikeStatusCheck'));
-
+        return redirect()->back()->with('errors', $this->model->errors())
+          ->withInput();
       } else {
 
-        $post = $this->request->getPost();
-        $payment = new Payment($post);
-        //$contract_number = $post['contract_number'];
+        //save payment info as a session variable so we can access values in sendConfirmationEmail
+        session()->set('payment', $post);
 
-        //validation
-        if ($this->model->save($payment) === false) {
+        $model->update($appointment->id, ['paid_rent' => 1]);
+        $appointment->paid_rent = 1;
 
-            return redirect()->back()->with('errors', $this->model->errors())
-                                     ->withInput();
-
-        } else {
-
-          //save payment info as a session variable so we can access values in sendConfirmationEmail
-          session()->set('payment', $post);
-
-          $model->update($appointment->id, ['paid_rent' => 1]);
-          $appointment->paid_rent = 1;
-
-          return redirect()->to(site_url('Admin/Payments/sendConfirmationEmail'));
-        }
-
+        return redirect()->to(site_url('Admin/Payments/sendConfirmationEmail'));
       }
     }
+  }
 
-    public function update($id)
-    {
-      $payment = $this->model->find($id);
+  public function update($id)
+  {
+    $payment = $this->model->find($id);
 
-      return view('Admin/Payments/update', ['payment' => $payment]);
+    return view('Admin/Payments/update', ['payment' => $payment]);
+  }
+
+  public function saveUpdate()
+  {
+    $payment = $this->request->getPost();
+
+    if ($this->model->save($payment)) {
+
+      return redirect()->to(site_url('Admin/Home/index'));
+    } else {
+
+      return redirect()->back()->with('errors', $this->model->errors());
     }
+  }
 
-    public function saveUpdate()
-    {
-      $payment = $this->request->getPost();
+  public function sendConfirmationEmail()
+  {
 
-      if($this->model->save($payment)) {
+    $appointment = session()->get('appointment');
 
-        return redirect()->to(site_url('Admin/Home/index'));
+    if ($appointment->sent_email === 1) {
 
-      } else {
-
-        return redirect()->back()->with('errors', $this->model->errors());
-        
-      }
-    }
-
-    public function sendConfirmationEmail() {
-
-      $appointment = session()->get('appointment');
-
-      if($appointment->sent_email === 1){
-
-        return redirect()->to(site_url('Admin/Appointments/bikeStatusCheck'));
-
-      } else {
+      return redirect()->to(site_url('Admin/Appointments/bikeStatusCheck'));
+    } else {
 
 
 
-        $model = new \App\Models\CustomersModel;
-        $customer = $model->where('id', $appointment->customer_id)->first();
-        $payment = session()->get('payment');
-        $amount = $payment['amount'] * 1000;
+      $model = new \App\Models\CustomersModel;
+      $customer = $model->where('id', $appointment->customer_id)->first();
+      $payment = session()->get('payment');
+      $amount = $payment['amount'] * 1000;
 
-        require ROOTPATH . '/vendor/PHPMailer-master/src/Exception.php';
-        require ROOTPATH . '/vendor/PHPMailer-master/src/PHPMailer.php';
-        require ROOTPATH . '/vendor/PHPMailer-master/src/SMTP.php';
+      require ROOTPATH . '/vendor/PHPMailer-master/src/Exception.php';
+      require ROOTPATH . '/vendor/PHPMailer-master/src/PHPMailer.php';
+      require ROOTPATH . '/vendor/PHPMailer-master/src/SMTP.php';
 
-        $mail = new PHPMailer(true);
-        $mail->isSMTP();
-        $mail->Host = 'mail.saigonbikerentals.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'patrick@saigonbikerentals.com';
-        $mail->Password = 'n1FaZ!Sz#)vB';
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 26;
-        $mail->setFrom('patrick@saigonbikerentals.com');
-        $mail->addAddress($customer->email_address);
-        $mail->isHTML(true);
-        $mail->Subject = 'Receipt for Rental Payment';
-        $mail->Body = '<p>Hi ' . trim($payment['customer_name'], "0..9") . ',</p>'
-                      . '<p>This message is to confirm receipt of ' . number_format($amount, 0, '.', ',') . ' đồng.</p>
+      $mail = new PHPMailer(true);
+      $mail->isSMTP();
+      $mail->Host = 'mail.saigonbikerentals.com';
+      $mail->SMTPAuth = true;
+      $mail->Username = 'patrick@saigonbikerentals.com';
+      $mail->Password = 'n1FaZ!Sz#)vB';
+      $mail->SMTPSecure = 'tls';
+      $mail->Port = 26;
+      $mail->setFrom('patrick@saigonbikerentals.com');
+      $mail->addAddress($customer->email_address);
+      $mail->isHTML(true);
+      $mail->Subject = 'Receipt for Rental Payment';
+      $mail->Body = '<p>Hi ' . trim($payment['customer_name'], "0..9") . ',</p>'
+        . '<p>This message is to confirm receipt of ' . number_format($amount, 0, '.', ',') . ' đồng.</p>
                       <p>Thanks for choosing Saigon Bike Rentals!</p>';
 
-        if (!$mail->send()) {
+      if (!$mail->send()) {
 
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+      } else {
 
-        } else {
-
-            $path = '{sng103.hawkhost.com:993/ssl}INBOX.Sent';
-            $imapStream = imap_open($path, 'patrick@saigonbikerentals.com', 'n1FaZ!Sz#)vB');
-            imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-            imap_close($imapStream);
-            echo 'Message sent!';
-
-        }
-
-        $appointment->sent_email = 1;
-
-        return redirect()->to(site_url('Admin/Appointments/bikeStatusCheck'));
-
+        $path = '{sng103.hawkhost.com:993/ssl}INBOX.Sent';
+        $imapStream = imap_open($path, 'patrick@saigonbikerentals.com', 'n1FaZ!Sz#)vB');
+        imap_append($imapStream, $path, $mail->getSentMIMEMessage());
+        imap_close($imapStream);
+        echo 'Message sent!';
       }
+
+      $appointment->sent_email = 1;
+
+      return redirect()->to(site_url('Admin/Appointments/bikeStatusCheck'));
     }
+  }
 }
