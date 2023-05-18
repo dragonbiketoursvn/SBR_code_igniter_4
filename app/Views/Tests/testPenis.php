45001807b1d0a26bd414ba0f72e873127ed8db67
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2023-05-18 10:27:23 +0700

    Update testPenis.php
    Testing CSS display: none in Safari

--- app/Views/Tests/testPenis.php
+++ app/Views/Tests/testPenis.php
@@ -9,7 +9,24 @@
 </head>
 
 <body>
-    <a href="https://api.whatsapp.com/send?phone=84972451273"><button>CALL SELF</button></a>
+    <div class="mailForm">
+
+        <div class="customerSelect">
+            <label for="names">Select Customer</label>
+            <select name="names" id="names">
+                <option value=""></option>
+                <option value="Franken Penis">Franken Penis</option>
+                <option value="Pussy Burger">Pussy Burger</option>
+            </select>
+        </div>
+        <input type="email" class="email">
+        <input type="textarea" class="messageBox">
+        <div class="buttons">
+            <button id="sendButton">Send</button>
+            <button id="mailCancelButton">Cancel</button>
+        </div>
+
+    </div>
 </body>
 
 </html>
