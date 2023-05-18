f767ed3c51089d97534047751549776ca34244e5
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2023-05-18 10:31:51 +0700

    Update testPenis.php
    Continued testing of CSS display: none in Safari

--- app/Views/Tests/testPenis.php
+++ app/Views/Tests/testPenis.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<?= $this->extend("layouts/default") ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Penis</title>
-</head>
+<?= $this->section('title') ?>Test Penis<?= $this->endSection() ?>
+
+<?= $this->section("content") ?>
 
 <body>
     <div class="mailForm">
