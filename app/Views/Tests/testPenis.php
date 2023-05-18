e83da7b0bc080f608f1305a856c2a0464a1dc904
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2023-05-18 11:15:50 +0700

    Update testPenis
    More testing of Safari issue

--- app/Views/Tests/testPenis.php
+++ app/Views/Tests/testPenis.php
@@ -4,41 +4,32 @@
 
 <?= $this->section("content") ?>
 
-<body>
-    <div class="mailForm">
+<div class="mailForm">
 
-        <div class="customerSelect">
-            <label for="names">Select Customer</label>
-            <select name="names" id="names">
-                <option value=""></option>
-                <option value="Franken Penis">Franken Penis</option>
-                <option value="Pussy Burger">Pussy Burger</option>
-            </select>
-        </div>
-        <input type="email" class="email">
-        <input type="textarea" class="messageBox">
-        <div class="buttons">
-            <button id="sendButton">Send</button>
-            <button id="mailCancelButton">Cancel</button>
-        </div>
-
+    <div class="customerSelect">
+        <label for="names">Select Customer</label>
+        <select name="names" id="names">
+            <option value=""></option>
+            <option value="Franken Penis">Franken Penis</option>
+            <option value="Pussy Burger">Pussy Burger</option>
+        </select>
+    </div>
+    <input type="email" class="email">
+    <input type="textarea" class="messageBox">
+    <div class="buttons">
+        <button id="sendButton">Send</button>
+        <button id="mailCancelButton">Cancel</button>
     </div>
 
-    <script>
-        const mailForm = document.querySelector(".mailForm");
+</div>
 
-        window.addEventListener('load', function(e) {
-            // fetch(url).then((response) => response.json()).then(function(json) {
-            //     emailJSON = json;
-            // });
-            mailForm.classList.add('hidden');
-            email.value = '';
-            nameInput.selectedIndex = 0;
-            messageBox.value = '';
+<script>
+    const mailForm = document.querySelector(".mailForm");
 
-            alert('penis face');
-        });
-    </script>
-</body>
+    window.addEventListener('load', function(e) {
+        mailForm.classList.add('hidden');
+        alert('penis face');
+    });
+</script>
 
-</html>
+<?= $this->endSection() ?>
