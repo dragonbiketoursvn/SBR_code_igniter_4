03a34cc70a765c95063deb217de9043b5f1349a5
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2023-08-03 22:18:57 +0700

    Update viewIndividual.php
    Added code to compress image files before form submission.

--- app/Views/Admin/Bikes/viewIndividual.php
+++ app/Views/Admin/Bikes/viewIndividual.php
@@ -836,6 +836,49 @@
     photoImages.forEach(e => e.style.filter = 'brightness(100%)');
 
   });
+
+  // code to compress image files before uploading via form
+  const fileInputs = document.querySelectorAll('input[type="file"]');
+
+  fileInputs.forEach(fileInput => {
+
+    const captureImage = (image) => {
+      const imageFile = fileInput.files[0];
+      const imageURL = URL.createObjectURL(imageFile);
+      image.src = imageURL;
+    };
+
+    const toBlobCallback = (blob) => {
+      const myFile = new File([blob], String(Date.now()) + ".jpg", {
+        type: blob.type,
+      });
+      const dataTransfer = new DataTransfer();
+      dataTransfer.items.add(myFile);
+      fileInput.files = dataTransfer.files; // fileInput referenced from outer scope
+      console.log(myFile);
+    };
+
+    // using as evt listener ensures the image has loaded before canvas is created
+    const prepareCanvasForCompression = (evt) => {
+      const image = evt.target;
+      const canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+      canvas.toBlob(toBlobCallback,
+        "image/jpeg",
+        0.8);
+    };
+
+    const compressAndAttachFile = (evt) => {
+      const image = document.createElement('img'); // needed as input by other callbacks
+      image.addEventListener('load', prepareCanvasForCompression); // only way to ensure img has loaded before canvas is created
+      captureImage(image); // captures imageFile from fileInput, creates and assigns URL to image.src
+    };
+
+    fileInput.addEventListener('change', compressAndAttachFile);
+  });
 </script>
 
 <?= $this->endSection() ?>
