54ce7b783b9169354965fc7ddd68c3f898eb7b57
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2023-09-02 17:45:30 +0700

    Break cache again

--- app/Views/Admin/Customers/newContractShort.php
+++ app/Views/Admin/Customers/newContractShort.php
@@ -475,6 +475,6 @@
 
       <script src="<?= site_url('js/Customers/newContract.js') ?>"></script>
 
-      <script src="<?= site_url('js/Customers/currencyConversion2.js') ?>"></script>
+      <script src="<?= site_url('js/Customers/currencyConversion.js') ?>"></script>
 
       <?= $this->endSection() ?>
