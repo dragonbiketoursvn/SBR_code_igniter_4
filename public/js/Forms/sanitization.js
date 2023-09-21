997c7de00fc0633796e314f9b4f4d33b7e05f60b
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2023-09-21 17:45:48 +0700

    Edit to trimInputValue() in sanitization.js
    As originally written this generated an error when used with form inputs to which files had been attached. Re-wrote to apply trimInputValue() to only inputs not of type="file"

--- public/js/Forms/sanitization.js
+++ public/js/Forms/sanitization.js
@@ -1,4 +1,4 @@
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll('input:not([type="file"])');
 
 const trimInputValue = (e) => {
   e.target.value = e.target.value.trim();
