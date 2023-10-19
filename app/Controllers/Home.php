144e769903d6d0f238ddb8072d24109b9ddd2d68
Patrick <dragonbiketoursvn@gmail.com>
2023-10-19 14:32:43 +0700

    testing

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-	// this is a new comment for testing purposes
+	// blah blah test blah blah
 	public function index()
 	{
 		return view('Home/index');
