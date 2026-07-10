0c092788cc9000fb220de5735ed4e0058446a996
Patrick Froehlich <dragonbiketoursvn@gmail.com>
2026-07-11 01:25:25 +0700

    testing

--- app/Controllers/Admin/PayPal.php
+++ app/Controllers/Admin/PayPal.php
@@ -4,10 +4,9 @@ namespace App\Controllers\Admin;
 
 class PayPal extends \App\Controllers\BaseController
 {
-
-
   public function payment()
   {
+    dd('blah');
     return view('Admin/PayPal/payment');
   }
 }
